8a85e3fe93d2dda4a60ad4377bf67b6bf9b1b6c6
Fredrick Otim <fredrickot@gmail.com>
2017-09-11 18:53:24 +0300

    add bootstrap

--- pickmeup/urls.py
+++ pickmeup/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
     url(r'^login/$', LoginView.as_view(), name='login'),
+    url(r'^profiles/', include('profiles.urls', namespace='profiles')),
     url(r'^items/', include('menus.urls', namespace='menus')),
     url(r'^resturants/', include('resturants.urls', namespace='resturants')),
     url(r'^about/$', TemplateView.as_view(template_name='about.html'), name='about'),
